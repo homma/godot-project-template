7f204aba7ad7294c728e289a721a4409f58f18cb
homma <dskhomma@gmail.com>
2025-07-28 23:51:50 +0900

    committed on 2025/07/28 23:51.

--- scripts/lib/utils.gd
+++ scripts/lib/utils.gd
@@ -1,11 +1,12 @@
 
-# load scene from a path and create instance
-static func load_scene(path):
-    return load(path).instantiate()
-
 # return a project setting value
 static func get_project_setting(path):
     return ProjectSettings.get_setting(path)
+
+# update a project setting value
+static func set_project_setting(path, value):
+    ProjectSettings.set_setting(path, value)
+    ProjectSettings.save()
 
 # create a node for a given class name
 static func create_node(name: StringName):
@@ -32,3 +33,22 @@ static func create_scene_from_node(root_node, scene_name):
     EditorInterface.open_scene_from_path(path)
 
     return path
+
+# load scene from a path and create instance
+static func load_scene(path):
+    return load(path).instantiate()
+
+# add a child node to a parent
+static func add_child(parent, child):
+    parent.add_child(child)
+
+    var owner = parent.get_owner()
+    if owner:
+        child.set_owner(owner)
+    else:
+        child.set_owner(parent)
+
+# add a node to a scene
+static func add_to_scene(root, parent, child):
+    parent.add_child(child)
+    child.set_owner(root)
