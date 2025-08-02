71ec21171e0b293841dd102aaabf24baedb27bd8
homma <dskhomma@gmail.com>
2025-08-02 15:38:01 +0900

    committed on 2025/08/02 15:38.

--- scripts/deploy/create_scene.gd
+++ scripts/deploy/create_scene.gd
@@ -2,38 +2,10 @@
 extends EditorScript
 
 const utils = preload("res://scripts/lib/utils.gd")
+const test = preload("res://scripts/lib/utils_test.gd")
 
 # Entry Point
 
 func _run():
     pass
-
-# Sample Functions
-
-func create_3d_scene_sample():
-
-    # root node
-    var root = utils.create_node("Node3D")
-    root.set_name("root")
-    root.show()
-
-    # child node
-    var child = utils.create_node("Node3D")
-    child.set_name("child")
-    utils.add_child(root, child)
-
-    utils.create_scene_from_node(root, "sample 3D scene")
-
-func create_2d_scene_sample():
-
-    # root node
-    var root = utils.create_node("Node2D")
-    root.set_name("root")
-    root.show()
-
-    # child node
-    var child = utils.create_node("Node2D")
-    child.set_name("child")
-    utils.add_child(root, child)
-
-    utils.create_scene_from_node(root, "sample 2D scene")
+    # test.create_circle_sample()
